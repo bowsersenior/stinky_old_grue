a7b11167b1ef090facf15eaea7c1b2e1295d65c5
Mani Tadayon <bowsersenior@gmail.com>
2012-10-23 03:25:02 -0700

    Use an observer for output to isolate game mechanics from user interaction

--- lib/basic_quest/game.rb
+++ lib/basic_quest/game.rb
@@ -1,56 +1,79 @@
-require 'io/console'
+require "observer"
 
 module BasicQuest
   class Game
-    attr_accessor :map, :teleport_room, :current_room
+    include Observable
+
+    VALID_INPUTS = %w(N E S W)
+
+    attr_accessor :map,
+                  :teleport_room,
+                  :current_room,
+                  :last_action,
+                  :last_direction,
+                  :turn
 
     def initialize(opts)
       self.map           = opts[:map]
       self.teleport_room = opts[:teleport_room]
-      self.current_room  = (self.map.keys - [self.teleport_room]).sample
-
-      output "Find 5 gems, then go to the #{self.teleport_room} Room"
+      self.turn          = 1
     end
 
-    def output(message)
-      puts message
-      puts "\n"
+    def start
+      self.current_room  = (self.map.keys - [self.teleport_room]).sample
+      self.broadcast :started
+    end
+
+    def prompt_for_direction
+      self.broadcast :prompt_for_direction
+    end
+
+    def go_through_door(direction)
+      self.last_direction = direction.to_sym
+      self.turn += 1
+
+      next_room = self.map[self.current_room][self.last_direction]
+      if next_room
+        self.current_room = next_room
+
+        if self.done?
+          self.broadcast :done
+        else
+          self.broadcast :changed_room
+        end
+      else
+        self.broadcast :found_locked_door
+      end
+    end
+
+    def rest
+      self.broadcast :take_a_rest
+      self.turn += 1
     end
 
     def done?
       self.current_room == self.teleport_room
     end
 
-    def next_move
-      Signal.trap("INT") do # SIGINT = control-C
-        exit
-      end
-
-      valid_inputs = %w(N E S W n e s w)
-      direction = nil
-
-      while ! valid_inputs.include?(direction) do
-        output "Currently in #{self.current_room} Room. Which way? (N E S W)"
-        direction = STDIN.getch
-      end
-
-      go_through_door(direction.upcase)
+    def valid_input?(d)
+      VALID_INPUTS.include?(d)
     end
 
-    private
-    def go_through_door(direction)
-      next_room = self.map[self.current_room][direction.to_sym]
-      if next_room
-        self.current_room = next_room
-        true
-      else
-        output "#{direction} door in #{self.current_room} Room is locked!"
-        false
-      end
-    rescue
-      require 'debugger'
-      debugger
-      nil
+    def to_hash
+      {
+        :current_room   => self.current_room,
+        :last_action    => self.last_action,
+        :teleport_room  => self.teleport_room,
+        :last_direction => self.last_direction,
+        :turn           => self.turn
+      }
+    end
+
+    # mark this object as changed & broadcast to observers
+    def broadcast(action)
+      self.last_action = action
+      self.changed
+      self.notify_observers(self.to_hash)
     end
   end
 end
---
+++ lib/basic_quest/output_observer.rb
@@ -0,0 +1,40 @@
+require "observer"
+
+module BasicQuest
+  module OutputObserver
+    MESSAGES = {
+      :started => lambda { |hsh|
+        "Find 5 gems, then go to the #{hsh  [:teleport_room]} Room."
+      },
+      :found_locked_door => lambda { |hsh|
+        "#{hsh[:last_direction]} door in #{hsh[:current_room]} Room is locked!"
+      },
+      :prompt_for_direction => lambda { |hsh|
+        "Currently in #{hsh[:current_room]} Room. Which way? (N E S W)"
+      },
+      :changed_room => lambda { |hsh|
+        "Went #{hsh[:last_direction]} to #{hsh[:current_room]} Room."
+      },
+      :take_a_rest => lambda { |hsh|
+        "Taking a rest on turn #{hsh[:turn]}"
+      },
+      :done => lambda { |hsh|
+        "Salutations! You reached the #{hsh[:teleport_room]} Room!"
+      }
+    }
+
+    def update(opts)
+      unless opts[:suppress_output]
+        template = MESSAGES[ opts[:last_action] ]
+        message  = template && template.call(opts)
+
+        if message
+          puts(message)
+          puts "\n"
+        end
+      end
+    end
+
+    module_function :update
+  end
+end
